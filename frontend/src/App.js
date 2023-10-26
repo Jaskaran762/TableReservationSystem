397ab4efce47eeb26f3c40bae3e7f990ed6da115
Gowri Prashanth Kanagaraj <gw351441@dal.ca>
2023-10-26 18:27:43 -0300

    login and signup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
-import {Auth} from './components/auth'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Auth from './components/login'
+import SignUp from './components/signup';
+import Home from './components/home';
+
 function App() {
   return (
-    <div className='App'>
-      <Auth/>
-      
-    </div>
+      <Router>
+      <div className='App'>
+      <Routes>
+    
+      <Route path="/" element={<Auth/>} />
+      <Route path="/signup" element={<SignUp/>}/>
+      <Route path="/home" element={<Home/>}/>
+     
+      </Routes>
+      </div>
+      </Router>
+ 
   );
 }
 
