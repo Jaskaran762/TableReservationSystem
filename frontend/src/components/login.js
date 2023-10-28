f91e43675e8ddabbfba8a1838d13a12dd28aee83
Jaskaran Singh <js356337@dal.ca>
2023-10-27 23:01:43 -0300

    Login issue resolved

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -1,54 +1,52 @@
-import { admin ,auth , googleProvider, db} from "../config/firebase";
-import { signInWithEmailAndPassword,signInWithPopup } from "firebase/auth";
-import { setDoc, doc } from 'firebase/firestore';
+import { admin, auth, googleProvider, db } from "../config/firebase";
+import { signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+import { setDoc, doc } from "firebase/firestore";
 import { useState } from "react";
-import { Link } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-function Auth(){
+function Auth() {
   const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   console.log(auth?.currentUser?.email);
-    var details
+  var details;
   const signIn = async () => {
     try {
-        await signInWithEmailAndPassword(auth, email, password).then((resp) => {
+      await signInWithEmailAndPassword(auth, email, password).then((resp) => {
         details = resp;
-    });
-        var user = details.user;
-        //console.log("Details->"+details);
-        console.log(JSON.stringify(user));
-        await setDoc(doc(db,"users",user.uid),{
-                email:user.email,
-                name:user.displayName
-            }
-        );
-    navigate('/home');
-    } catch (err){
+      });
+      var user = details.user;
+      //console.log("Details->"+details);
+      console.log(JSON.stringify(user));
+      await setDoc(doc(db, "users", user.uid), {
+        email: user.email,
+        name: user.displayName,
+      });
+      navigate("/home");
+    } catch (err) {
       console.error(err);
     }
   };
 
   const signInWithGoogle = async () => {
     try {
-     await signInWithPopup(auth,googleProvider).then((resp) =>{
+      await signInWithPopup(auth, googleProvider).then((resp) => {
         details = resp;
-    });
-    var user = details.user;
-    //console.log("Details->"+details);
-    console.log(JSON.stringify(user));
-        await setDoc(doc(db,"users",user.uid),{
-            email:user.email,
-            name:user.displayName
-        }
-  );
-    navigate('/home')
-    } catch (err){
+      });
+      var user = details.user;
+      //console.log("Details->"+details);
+      console.log(JSON.stringify(user));
+      await setDoc(doc(db, "users", user.uid), {
+        email: user.email,
+        name: user.displayName,
+      });
+      navigate("/home");
+    } catch (err) {
       console.error(err);
     }
   };
-  
+
   // const logOut = async () => {
   //   try {
   //   await signOut(auth);
@@ -58,46 +56,21 @@ function Auth(){
   // };
 
   return (
-    <div className="container">
-      <div className="sidebar">
-        <h1>Restaurant Search</h1>
-        <form onSubmit={handleSubmit}>
-          <div>
-            <label>City: </label>
-            <input type="text" value={city} onChange={(e) => setCity(e.target.value)} />
-          </div>
-          <div>
-            <label>Name: </label>
-            <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-          </div>
-          <div>
-            <label>Rating (Min): </label>
-            <input type="number" value={rating} onChange={(e) => setRating(e.target.value)} />
-          </div>
-          <button type="submit">Search</button>
-        </form>
-        {error && <div>Error: {error.message}</div>}
-      </div>
-      <div className="content">
-        <ul>
-          {restaurants.map((restaurant) => (
-            <li key={restaurant.name} className="res">
-              {/* Wrap the contents in a Link component */}
-              <Link to="/restaurant">
-                <img src={restaurant.photo} alt={restaurant.name} width="100" />
-                <div>Name: {restaurant.name}</div>
-                <div>City: {restaurant.city}</div>
-                <div>Location: {restaurant.location}</div>
-                <div>Rating: {restaurant.resRating}</div>
-                <div>Opening Time: {restaurant.openingTime}</div>
-                <div>Closing Time: {restaurant.closingTime}</div>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
+    <div className="form-container">
+      <input placeholder="Email.." onChange={(e) => setEmail(e.target.value)} />
+      <input
+        type="password"
+        placeholder="Password.."
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={signIn}> Sign In</button>
+      <Link to="/signup">
+        <button> Sign Up</button>
+      </Link>
+      <button onClick={signInWithGoogle}>Continue With Google</button>
+      {/* <button onClick={logOut}> logOut</button> */}
     </div>
   );
 }
 
-export default Home;
+export default Auth;
