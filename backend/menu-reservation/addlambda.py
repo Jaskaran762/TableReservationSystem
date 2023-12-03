c46f53ef6c01f1bab59538431788ff26b9b6c136
ssb <ss246800@dal.ca>
2023-12-03 00:24:04 -0400

    update lambda files for add and delete the menu items

--- backend/menu-reservation/addlambda.py
+++ backend/menu-reservation/addlambda.py
@@ -5,20 +5,26 @@ dynamodb=boto3.client('dynamodb')
 
 def lambda_handler(event, context):
     try:
-        table_name = 'carttable'  # Replace with your DynamoDB table name
+        table_name = 'carttable'  
         data = json.loads(event['body'])
+        print('Data:', data)  # Add print statement here
 
         # Extract menu item details from the request
         menu_name = data.get('MenuName', '')
-        reservation_id = data.get('ReservId', '')
-        user_id = data.get('UserId', '')
+        rest_name=data.get('RestaurantName', '')
+        # reservation_id = data.get('ReservId', '')
+        user_name = data.get('UserName', '')
         quantity = data.get('Quantity', '')
-      
+        print('Menu:', menu_name)
+        print('Restaurant:', rest_name)
+        print('User:', user_name)
+        print('Quantity:', quantity)
         # Create an item to put in the DynamoDB table
         item = {
             'MenuName': {'S': menu_name},
-            'ReservId': {'S': reservation_id},
-            'UserId': {'S': user_id},
+            'RestaurantName': {'S': rest_name},
+            # 'ReservId': {'S': reservation_id},
+            'UserName': {'S': user_name},
             'Quantity': {'N': quantity},
         }
 
@@ -30,6 +36,7 @@ def lambda_handler(event, context):
             'body': json.dumps({'message': 'Menu item added successfully'})
         }
     except Exception as e:
+        print('Error:', e)  # Add print statement for error
         return {
             'statusCode': 500,
             'body': json.dumps({'message': 'Internal Server Error', 'error': str(e)})
